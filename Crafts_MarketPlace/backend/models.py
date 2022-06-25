0e8de4713b1ba9cac3e42ee4b0907a9610bd26c1
Kumar Spandan Pattanayak <75578279+5p7Ro0t@users.noreply.github.com>
2022-06-26 01:54:11 +0530

    Update models.py

--- Crafts_MarketPlace/backend/models.py
+++ Crafts_MarketPlace/backend/models.py
@@ -1,21 +1,22 @@
 from django.db import models
+#Create your models here.
 
-# Create your models here.
 class SellerData(models.Model):
     # unique_id = models.CharField(max_length=20, editable=False, unique=True,default="")
     fname = models.CharField(max_length=50, default="")
     lname = models.CharField(max_length=50, default="")
     email_id = models.EmailField(max_length=50, unique=True)
     phone = models.CharField(max_length=10, unique=True,null=False,blank=False,default=0)
-    
-        
+    username = models.CharField(max_length=20,unique=True, default='')
+    password = models.CharField(max_length=20,default="")
 
 class CustomerData(models.Model):
     fname = models.CharField(max_length=50, default="")
     lname = models.CharField(max_length=50, default="")
     email_id = models.EmailField(max_length=50, unique=True)
     phone = models.CharField(max_length=10,unique=True,null=False,blank=False,default=0)
-
+    username = models.CharField(max_length=20,unique=True, default='')
+    password = models.CharField(max_length=20,default="")
 
 class ProductsData(models.Model):
     name = models.CharField(max_length=50, default="")
@@ -24,4 +25,5 @@ class ProductsData(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     isAvailable = models.BooleanField(null=False, default=False)
     discount = models.DecimalField(max_digits=3, decimal_places=2)
+    state = models.CharField(max_length=15,default="")
     
